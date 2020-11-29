0cf8c784556c5d5125b9e95f8de56d06e5eba7eb
zazyzaya <zazyzaya@gmail.com>
2020-11-29 14:09:48 -0500

    added runall

---
+++ dinero_cmds/run_all.sh
@@ -0,0 +1,8 @@
+array=( cora_w_data cora_no_data citeseer_w_data citeseer_no_data )
+for i in "${array[@]}"
+do 
+    for f in [^run_all]*.sh
+    do 
+        echo "$f $i.din $i.out"
+    done
+done 
