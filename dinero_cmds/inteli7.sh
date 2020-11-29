957bfadba5f968264e9414b5dda66cc0f2ff8a9b
zazyzaya <zazyzaya@gmail.com>
2020-11-29 13:53:27 -0500

    adding scripts

---
+++ dinero_cmds/inteli7.sh
@@ -0,0 +1,5 @@
+./dineroIV \
+-l1-dsize 32k -l1-dbsize 64 -l1-dassoc 8 -l1-drepl l \
+-l1-isize 32k -l1-ibsize 64 -l1-iassoc 8 -l1-irepl l \
+-l2-usize 256k -l2-ubsize 64 -l2-uassoc 8 -l2-urepl l \
+-informat d < ArchPaper/dinfiles/$1 > ArchPaper/i7/$2
